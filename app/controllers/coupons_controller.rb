91d98be62034fbdf7ee9d9f73bf186cf5dac997d
Mukunda Gogoi <ieglonew01f@gmail.com>
2016-08-06 02:21:58 +0530

    fixin timestamp logic

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -2,9 +2,15 @@ class CouponsController < InheritedResources::Base
   def index
       #get coupons_release_date
       release_date = Setting.find_by_name('coupons_release_date').value
-      ds = release_date.split(' ')[0]
 
-      date_passed = Date.parse(ds) < Date.today
+      ds = release_date.split(' ')
+      d1 = ds[0].split('-')
+      d2 = ds[1].split(':')
+
+      release_date_timestamp = Time.new(d1[0].to_i, d1[1].to_i, d1[2].to_i, d2[0].to_i, d2[1].to_i, '+5:30').to_i
+      current_time_stamp = Time.now.getlocal('+05:30').to_i
+
+      date_passed = release_date_timestamp <= current_time_stamp
 
       if date_passed
         #get coupon code where active is 1
@@ -23,7 +29,7 @@ class CouponsController < InheritedResources::Base
           coupon.save
           render json: {status: 200, coupon: code}
         else
-          render json: {status: 202, coupon: 'Sorry someone already took the coupon'}
+          render json: {status: 202, coupon: 'Sorry someone already took the coupon, better luck next time :)'}
         end
       else
         render json: {status: 422, coupon: 'No coupon for you'}
